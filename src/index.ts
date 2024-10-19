e5f5683ac403a893b7a28783918f304c012bf107
Nikhil Kumar <kumarnikhil48578@gmail.com>
2024-10-19 15:15:25 +0530

    Add morgan logging middleware

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,31 @@
 import express, { Application,Request,Response } from "express"
 import "dotenv/config"
+import logger from "./utils/logger.js";
+import morgan from "morgan";
 
+const morganFormat = ":method :url :status :response-time ms";
 
 const app: Application = express()
 const PORT = process.env.PORT || 5050
 
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
+
 app.listen(PORT, () => {
-    console.log(`Server is running...${PORT}`);
+    logger.info(`Server is running...${PORT}`);
     
 })
 
---
+++ src/utils/logger.ts
@@ -0,0 +1,33 @@
+import { createLogger, format, transports } from "winston";
+const { combine, timestamp, json, colorize } = format;
+
+// Custom format for console logging with colors
+const consoleLogFormat = format.combine(
+  format.colorize(),
+  format.printf(({ level, message, timestamp }) => {
+    return `${level}: ${message}`;
+  })
+);
+
+// Create a Winston logger
+const logger = createLogger({
+  level: "info",
+  format: combine(colorize(), timestamp(), json()),
+  transports: [
+    new transports.Console({
+      format: consoleLogFormat,
+    }),
+    new transports.File({
+        filename: "app.log",
+        dirname: "logs",
+        level: "info"
+    }),
+    new transports.File({
+        filename: "error.log",
+        dirname: "logs",
+        level: "error"
+    }),
+  ],
+});
+
+export default logger;
