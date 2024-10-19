aa3201e67f460b74012d4bfc496a5cf1ea956d84
Nikhil Kumar <kumarnikhil48578@gmail.com>
2024-10-19 21:15:15 +0530

    create user model

---
+++ src/models/auth.model.ts
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        maxlength: [20, "Username cannot exceed 15 characters"],
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        match: [/.+\@.+\..+/, "Please fill a valid email address"],
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    avatar: {
+        type: String
+    }
+    },
+    {
+    timestamps: true   
+    }
+)
+
+export const User = mongoose.model("User", userSchema)
