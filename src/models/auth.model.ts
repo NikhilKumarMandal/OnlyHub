eca3754bd637857352989f3141d9072361a24715
Nikhil Kumar <kumarnikhil48578@gmail.com>
2024-10-20 23:06:47 +0530

    Add jsonwebtoken dependency and update user model

--- src/models/auth.model.ts
+++ src/models/auth.model.ts
@@ -1,9 +1,11 @@
 import mongoose, { Document } from "mongoose";
-
+import jwt from "jsonwebtoken"
 export interface IUser extends Document {
     username: string;
     email: string;
     password: string;
+    generateAccessToken: () => Promise<string>; 
+    generateRefreshToken: () => Promise<string>;
 }
 
 const userSchema = new mongoose.Schema({
@@ -25,7 +27,7 @@ const userSchema = new mongoose.Schema({
     },
     password: {
         type: String,
-        required: true
+        required: [true,"Password is requried!"]
     },
     avatar: {
         type: String
@@ -35,5 +37,25 @@ const userSchema = new mongoose.Schema({
     timestamps: true   
     }
 )
+
+userSchema.methods.generateAccessToken = async function (): Promise<string> {
+    const token = jwt.sign({
+        id: this._id
+    }, process.env.ACCESS_TOKEN_SECERT,
+    {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+    });
+    return token;
+};
+
+userSchema.methods.generateRefreshToken = async function (): Promise<string> {
+    const token = jwt.sign({
+        id: this._id
+    }, process.env.REFRESH_TOKEN_SECERT,
+    {
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+    });
+    return token;
+};
 
 export const User = mongoose.model<IUser>("User", userSchema);
